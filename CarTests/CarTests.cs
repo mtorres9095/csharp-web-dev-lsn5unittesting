4216ecaa907564a49aad69abb5ac851037aea1e2
mtorres9095 <mtorres9095@gmail.com>
2020-06-09 14:03:33 -0400

    Chapter 6 Exercise

--- CarTests/CarTests.cs
+++ CarTests/CarTests.cs
@@ -1,28 +1,64 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarNS;
+
+
 
 namespace CarTests
 {
-    [TestClass]
-    public class CarTests
+    public class CarTest
     {
-        //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitHub account)
-       [TestMethod]
-       public void EmptyTest()
+        //TODO: add emptyTest so we can configure our runtime environment
+        [TestMethod]
+        public void EmptyTest()
         {
             Assert.AreEqual(10, 10, .001);
         }
-        //TODO: constructor sets gasTankLevel properly
-        CarTests test_car;
-                
+
+        Car test_car;
+
         [TestInitialize]
         public void CreateCarObject()
         {
-            //Car test_car = new Car("Toyota", "Prius", 10, 50);
             test_car = new Car("Toyota", "Prius", 10, 50);
         }
+
+        //TODO: constructor sets gasTankLevel properly
+        [TestMethod]
+        public void TestInitialGasTank()
+        {
+            test_car = new Car("Toyota", "Prius", 10, 50);
+            Assert.AreEqual(10, test_car.GasTankLevel, .001);
+        }
+
         //TODO: gasTankLevel is accurate after driving within tank range
-        //TODO: gasTankLevel is accurate after attempting to drive past tank range
+        [TestMethod]
+        public void TestGasTankAfterDriving()
+        {
+            test_car.Drive(50);
+            Assert.AreEqual(9, test_car.GasTankLevel, .001);
+        }
+        [TestMethod]
+        public void TestGasTankAfterExceedingTankRange()
+        {
+            test_car.Drive(500);
+            Assert.AreEqual(0, test_car.GasTankLevel, .001);
+
+        }
         //TODO: can't have more gas than tank size, expect an exception
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentOutOfRangeException))]
+        public void TestGasOverfillException()
+        {
+            test_car.AddGas(5);
+            Assert.Fail("Shouldn't get here, car cannot have more gas in tank than the size of the tank");
+        }
+
 
     }
+
 }
+
+
+
+
+
