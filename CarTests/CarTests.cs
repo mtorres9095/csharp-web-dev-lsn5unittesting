4c7d664819d39c1d249ba6d13366d9c66c1565e2
mtorres9095 <mtorres9095@gmail.com>
2020-06-08 22:44:40 -0400

    initial Set Up

--- CarTests/CarTests.cs
+++ CarTests/CarTests.cs
@@ -2,10 +2,24 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CarTests
 {
+    [TestClass]
     public class CarTests
     {
         //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitHub account)
+       [TestMethod]
+       public void EmptyTest()
+        {
+            Assert.AreEqual(10, 10, .001);
+        }
         //TODO: constructor sets gasTankLevel properly
+        CarTests test_car;
+                
+        [TestInitialize]
+        public void CreateCarObject()
+        {
+            //Car test_car = new Car("Toyota", "Prius", 10, 50);
+            test_car = new Car("Toyota", "Prius", 10, 50);
+        }
         //TODO: gasTankLevel is accurate after driving within tank range
         //TODO: gasTankLevel is accurate after attempting to drive past tank range
         //TODO: can't have more gas than tank size, expect an exception
